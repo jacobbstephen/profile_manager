eb5a36cd6d93a22c8a31a3e1ae1cc49b2c27af44
JACOB B STEPHEN <94170866+jacobbstephen@users.noreply.github.com>
2025-06-17 11:28:28 +0530

    Delete ignite2025/profile directory

--- ignite2025/profile/CSVHeaders.java
+++
@@ -1,8 +0,0 @@
-package ignite2025.profile;
-
-public interface CSVHeaders {
-	int NAME = 0;
-	int EMPLOYEE_ID = 1;
-	int COMPANY = 2;
-	int DESIGINATION = 3;
-}
--- ignite2025/profile/Path.java
+++
@@ -1,5 +0,0 @@
-package ignite2025.profile;
-
-public interface Path {
-	String SOURCE_FILE_PATH = "C:/Users/jacob/Downloads/profiles.txt";
-}
